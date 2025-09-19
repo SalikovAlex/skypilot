333c1888afb5065d495571cc0b9952e01f17ccac
Romil Bhardwaj <romil.bhardwaj@gmail.com>
2025-09-19 14:20:19 -0700

    remove nvidia autoscaler

--- sky/provision/kubernetes/utils.py
+++ sky/provision/kubernetes/utils.py
@@ -1259,8 +1259,17 @@ class CoreweaveAutoscaler(Autoscaler):
     label_formatter: Any = CoreWeaveLabelFormatter
     can_query_backend: bool = False
 
+
 class NvidiaAutoscaler(Autoscaler):
     """Generic autoscaler for clusters using Nvidia GFD
+    """
+
+    label_formatter: Any = GFDLabelFormatter
+    can_query_backend: bool = False
+
+
+class NebiusAutoscaler(Autoscaler):
+    """Nebius autoscaler.
     """
 
     label_formatter: Any = NebiusLabelFormatter
@@ -1280,6 +1289,7 @@ AUTOSCALER_TYPE_TO_AUTOSCALER = {
     kubernetes_enums.KubernetesAutoscalerType.GKE: GKEAutoscaler,
     kubernetes_enums.KubernetesAutoscalerType.KARPENTER: KarpenterAutoscaler,
     kubernetes_enums.KubernetesAutoscalerType.COREWEAVE: CoreweaveAutoscaler,
+    kubernetes_enums.KubernetesAutoscalerType.NEBIUS: NebiusAutoscaler,
     kubernetes_enums.KubernetesAutoscalerType.NVIDIA: NvidiaAutoscaler,
     kubernetes_enums.KubernetesAutoscalerType.GENERIC: GenericAutoscaler,
 }
