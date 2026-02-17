5a55d4d9fc535d2993c47075d42430743e99b201
Alexander Salikov <Salikov57@gmail.com>
2026-02-17 10:51:36 +0000

    Make `get_accelerator_from_label_value` return upper values to work with h100 and H100

--- sky/provision/kubernetes/utils.py
+++ sky/provision/kubernetes/utils.py
@@ -793,7 +793,7 @@ class NebiusLabelFormatter(GPULabelFormatter):
 
     @classmethod
     def get_accelerator_from_label_value(cls, value: str) -> str:
-        return value.lower()
+        return value.upper()
 
     @classmethod
     def validate_label_value(cls, value: str) -> Tuple[bool, str]:
