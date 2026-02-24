7fc8863a1eefeac0ecef335adadc42a8624820f3
Alexander Salikov <Salikov57@gmail.com>
2026-02-24 13:27:23 +0000

    Add support for configurable disk tiers in Nebius

--- sky/provision/nebius/utils.py
+++ sky/provision/nebius/utils.py
@@ -188,7 +188,7 @@ def launch(cluster_name_on_cloud: str,
            user_data: str,
            associate_public_ip_address: bool,
            filesystems: List[Dict[str, Any]],
-           disk_tier: resources_utils.DiskTier,
+           disk_tier: str,
            use_static_ip_address: bool = False,
            use_spot: bool = False,
            network_tier: Optional[resources_utils.NetworkTier] = None) -> str:
@@ -233,7 +233,7 @@ def launch(cluster_name_on_cloud: str,
                 cluster_id = get_or_create_gpu_cluster(cluster_name_on_cloud,
                                                        project_id, fabric)
 
-    def _disk_tier_to_disk_type(disk_tier: resources_utils.DiskTier) -> Any:
+    def _disk_tier_to_disk_type(disk_tier: str) -> Any:
         tier2type = {
             str(resources_utils.DiskTier.HIGH):
                 nebius.compute().DiskSpec.DiskType.NETWORK_SSD_IO_M3,
